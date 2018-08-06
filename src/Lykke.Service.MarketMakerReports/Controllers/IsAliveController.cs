69a92500567c67afe2b1e1b141fcee5a33af6f85
Andrey Andreev <AndreevAndrey@ya.ru>
2018-08-06 11:46:01 +0300

    AuditMessage subscriber

---
+++ src/Lykke.Service.MarketMakerReports/Controllers/IsAliveController.cs
@@ -0,0 +1,11 @@
+﻿using Lykke.Sdk.Health;
+
+namespace Lykke.Service.MarketMakerReports.Controllers
+{
+    public class IsAliveController : Sdk.Controllers.IsAliveController
+    {
+        public IsAliveController(IHealthService healthService) : base(healthService)
+        {
+        }
+    }
+}
