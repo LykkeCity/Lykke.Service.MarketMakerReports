6110c163282ee24304ac1bab6adf8821f769ae6e
Andrey Andreev <AndreevAndrey@ya.ru>
2018-08-07 09:16:57 +0300

    Mark IAuditMessagesApi Get method with Refit Get attribute

--- client/Lykke.Service.MarketMakerReports.Client/Api/IAuditMessagesApi.cs
+++ client/Lykke.Service.MarketMakerReports.Client/Api/IAuditMessagesApi.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Lykke.Service.MarketMakerReports.Client.Models;
+using Refit;
 
 namespace Lykke.Service.MarketMakerReports.Client.Api
 {
     public interface IAuditMessagesApi
     {
+        [Get("/api/auditmessages")]
         Task<IReadOnlyList<AuditMessageModel>> Get(DateTime? from, DateTime? to, string clientId = null);
     }
 }
