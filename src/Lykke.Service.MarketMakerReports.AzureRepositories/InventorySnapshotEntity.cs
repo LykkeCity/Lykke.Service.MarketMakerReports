e98938c6bafcf531dcd412f615c3c7558ccfb8ab
Andrey Andreev <AndreevAndrey@ya.ru>
2018-08-09 17:48:23 +0300

    Remove alternative storing method of InventorySnapshot with several lines per snapshot. Go with one simple Json field instead.

--- src/Lykke.Service.MarketMakerReports.AzureRepositories/InventorySnapshotEntity.cs
+++ src/Lykke.Service.MarketMakerReports.AzureRepositories/InventorySnapshotEntity.cs
@@ -16,23 +16,5 @@ namespace Lykke.Service.MarketMakerReports.AzureRepositories
         }
         
         public string Json { get; set; }
-        
-//        public string Source { get; set; }
-//        
-//        public DateTime Timestamp { get; set; }
-//        
-//        public string Asset { get; set; }
-//        
-//        public string Exchange { get; set; }
-//        
-//        public decimal UsdEquivalent { get; set; }
-//        
-//        public decimal Volume { get; set; }
-//        
-//        public decimal SellVolume { get; set; }
-//        
-//        public decimal BuyVolume { get; set; }
-//        
-//        public decimal Amount { get; set; }
     }
 }
