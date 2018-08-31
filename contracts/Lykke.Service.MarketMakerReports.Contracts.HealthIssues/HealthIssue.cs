9b887f8bf2be64da5d4f50bfcb1420a2f4b35297
Andrey Andreev <AndreevAndrey@ya.ru>
2018-08-31 11:06:13 +0300

    LIQ-601 fix Check in forgotten files

---
+++ contracts/Lykke.Service.MarketMakerReports.Contracts.HealthIssues/HealthIssue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lykke.Service.MarketMakerReports.Contracts.HealthIssues
+{
+    public class HealthIssue
+    {
+        public string Source { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string Type { get; set; }
+
+        public string Message { get; set; }
+    }
+}
