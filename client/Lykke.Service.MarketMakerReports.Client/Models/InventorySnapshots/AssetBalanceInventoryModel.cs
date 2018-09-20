7e05383110c1a59fda32239c30a8fdc7da1d7cb8
Petr Giloian <giloyanp@gmail.com>
2018-09-20 19:58:39 +0700

    LIQ-729 Added AssetDisplayId and Assetid properties to inventory snapshot model.

--- client/Lykke.Service.MarketMakerReports.Client/Models/InventorySnapshots/AssetBalanceInventoryModel.cs
+++ client/Lykke.Service.MarketMakerReports.Client/Models/InventorySnapshots/AssetBalanceInventoryModel.cs
@@ -1,11 +1,13 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Lykke.Service.MarketMakerReports.Client.Models.InventorySnapshots
 {
     public class AssetBalanceInventoryModel
     {
-        public string Asset { get; set; }
-        
+        public string AssetId { get; set; }
+
+        public string AssetDisplayId { get; set; }
+
         public IReadOnlyList<AssetInventoryModel> Inventories { get; set; }
         
         public IReadOnlyList<AssetBalanceModel> Balances { get; set; }
